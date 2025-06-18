2bfba893f0d87b0e65fb40327bb6d67000559c92
causeri3 <vanessaca@web.de>
2025-06-18 10:18:51 +0200

    working

--- installation.py
+++ installation.py
@@ -1,12 +1,18 @@
 import logging
-from utils.video import Stream
+from yolo_v8_face.utils.video import Stream
 import cv2
-from PIL import Image
-from datetime import datetime
-from yolo_v8_face.utils.transformation_screen import show_loader, play_gif_frames
+import threading
+import numpy as np
+from neural_style_transfer.nst import generate_image_list
+
+from installation_utils.utils import (request_sdxlturbo
+                                      )
 # from utils.args import get_args
 #
 # args, unknown = get_args()
+
+
+WIDTH, HEIGHT   = (512, 512)
 
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
@@ -18,18 +24,81 @@ logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 # window_name = 'SELFIE'
 # PROMPT = "dmt"
 
-stream = Stream(
-    #see_detection=args.see_detection,
-    #available_devices = [0],
-    see_detection=True,
-    available_devices=None
-    )
-selfie = stream.draw_boxes()
-#cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
-#cv2.imshow(window_name, selfie)
-# cv2.waitKey(1)
-# cv2.destroyWindow(window_name)
-# cv2.waitKey(1)
 
-stamp = datetime.now().strftime("%-d_%B_%H_%M").lower()
-cv2.imwrite("yolo_v8_face/images/selfie" + '_' + stamp + ".png", selfie)
+frames = []
+gif_lock = threading.Lock()
+new_frames_event = threading.Event()
+selfie_lock = threading.Lock()
+selfie_ready_event = threading.Event()
+selfie_image = None
+
+def processing_worker(selfie):
+    sdxl_result = {}
+    sdxl_thread = threading.Thread(target=request_sdxlturbo, args=(selfie, sdxl_result))
+    sdxl_thread.start()
+
+    logging.info("Generating NST images...")
+    nst_images = generate_image_list(selfie)
+
+    sdxl_thread.join(timeout=600)
+
+    if sdxl_result.get('success', False):
+        gif_frames = [img for img in sdxl_result['images']]
+    else:
+        gif_frames = [np.array(img) for img in nst_images]
+
+    with gif_lock:
+        global frames
+        frames = gif_frames
+        new_frames_event.set()
+
+def selfie_capture_worker():
+    global selfie_image
+    stream = Stream(see_detection=False, available_devices=[0])
+
+    selfie = stream.draw_boxes()  # This is blocking, but now it runs in a thread
+    logging.info("Selfie captured.")
+
+    with selfie_lock:
+        selfie_image = selfie
+        selfie_ready_event.set()
+
+def main_loop():
+    frame_index = 0
+    is_generating = False
+
+    # Start first selfie capture thread
+    threading.Thread(target=selfie_capture_worker).start()
+
+    while True:
+        # If selfie ready and no processing running → start processing worker
+        if selfie_ready_event.is_set() and not is_generating:
+            with selfie_lock:
+                selfie = selfie_image
+            threading.Thread(target=processing_worker, args=(selfie,)).start()
+            is_generating = True
+            selfie_ready_event.clear()
+
+        # Draw gif frame or noise
+        with gif_lock:
+            if frames:
+                frame = frames[frame_index % len(frames)]
+                frame_index += 1
+            else:
+                frame = np.random.randint(0, 256, (512, 512, 3), dtype=np.uint8)
+
+        cv2.imshow("TRANSFORMATION", frame)
+        key = cv2.waitKey(100)
+        if key == 27:  # ESC to quit
+            break
+
+        # If new gif frames ready → start new selfie capture
+        if new_frames_event.is_set():
+            is_generating = False
+            new_frames_event.clear()
+            threading.Thread(target=selfie_capture_worker).start()
+
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main_loop()
