1b7993aaf27d4e87aea6ebdfc5cbf8d8ffe2d4dd
causeri3 <vanessaca@web.de>
2025-06-19 12:11:06 +0200

    documentation

--- yolo_v8_face/main.py
+++ yolo_v8_face/main.py
@@ -1,9 +1,9 @@
+""" Script is not needed, just here if you want to test the selfie functionality separated."""
+
 import logging
 from utils.video import Stream
 import cv2
-from PIL import Image
 from datetime import datetime
-from utils.transformation_screen import show_loader, play_gif_frames
 # from utils.args import get_args
 #
 # args, unknown = get_args()
@@ -16,11 +16,10 @@ logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 # else:
 #     device = None
 # window_name = 'SELFIE'
-# PROMPT = "dmt"
 
 stream = Stream(
     #see_detection=args.see_detection,
-    #available_devices = [0],
+    #available_devices = device, # [0],
     see_detection=True,
     available_devices=None
     )
