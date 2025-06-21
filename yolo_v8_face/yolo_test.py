69e844abdd3041196e5dfb556d9135a8bdd83822
causeri3 <vanessaca@web.de>
2025-06-21 10:01:51 +0200

    make yolo test script runnable in its directory

--- yolo_v8_face/yolo_test.py
+++ yolo_v8_face/yolo_test.py
@@ -1,29 +1,33 @@
 """ Script is not needed, just here if you want to test the selfie functionality separated."""
 
+import os
+import sys
+
+# pretend you are in the directory above
+try:
+    this_file = __file__
+except NameError:
+    this_file = sys.argv[0]
+project_dir = os.path.abspath(os.path.join(os.path.dirname(this_file), '..'))
+sys.path.insert(0, project_dir)
+
 import logging
-from utils.video import Stream
+from yolo_v8_face.utils.video import Stream
 import cv2
 from datetime import datetime
-# from utils.args import get_args
-#
-# args, unknown = get_args()
+
 
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
-# if args.cam_device_number:
-#     logging.info("""You chose camera device no: {}""".format(args.cam_device_number))
-#     device = [args.cam_device_number]
-# else:
-#     device = None
-# window_name = 'SELFIE'
 
 stream = Stream(
-    #see_detection=args.see_detection,
     #available_devices = device, # [0],
     see_detection=True,
     available_devices=None
     )
 selfie = stream.draw_boxes()
+
+# window_name = 'SELFIE'
 #cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
 #cv2.imshow(window_name, selfie)
 # cv2.waitKey(1)
